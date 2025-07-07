96e1a4d9a0e89b268ba63d08f862c49464abca45
jaymonpike <jaymonpike@hotmail.com>
2025-07-07 23:39:10 +0000

    ex0801

---
+++ src/CMakeFiles/ex0802.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex0802.dir/ex0802.cpp.o"
+  "CMakeFiles/ex0802.dir/ex0802.cpp.o.d"
+  "ex0802"
+  "ex0802.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex0802.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
